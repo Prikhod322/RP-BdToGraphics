93f51902a3293ba67aec2b8b7d64c30baf8fcf10
Prikhod322 <vovkaprikhod@gmail.com>
2022-05-18 14:46:10 +0300

    Добавьте файлы проекта.

---
+++ WebApplication15/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication15.Models;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace WebApplication15.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public List<User> users = new List<User>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Dapper()
+        {
+            using (MySqlConnection connection = new MySqlConnection($@"Data Source=database-1.cqrandacnrdj.eu-west-2.rds.amazonaws.com,3306;User ID=admin;Password=12345qwert;Database=DB1"))
+            {
+                users = connection.Query<User>("SELECT * FROM USER").ToList();
+                connection.Open();
+                connection.Close();
+            }
+
+            return View(users);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication15/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication15.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Login{ get; set; }
+        public string Email { get; set; }
+        public string HashPass { get; set; }
+
+        public User()
+        {
+            ID = 0;
+            Login = Email = HashPass = string.Empty;
+        }
+    }
+}
